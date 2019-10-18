5cef7a62fc8e57b03dd214bf329b2430b00dcd45
EJ <ejg132@gmail.com>
2019-10-17 23:12:48 -0700

    updated endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://limitless-sierra-11500.herokuapp.com/',
   TOKEN_KEY: 'lost-puppers-client-auth-token',
   USER_NAME: 'lost-puppers-user-name',
   USER_ID: 'lost-puppers-user-id'
