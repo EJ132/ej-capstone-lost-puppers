12cc4c632f64a257b7b3353e0c9f0704c4729116
EJ <ejg132@gmail.com>
2019-10-17 23:17:06 -0700

    / doing a double /

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'https://limitless-sierra-11500.herokuapp.com/',
+  API_ENDPOINT: 'https://limitless-sierra-11500.herokuapp.com',
   TOKEN_KEY: 'lost-puppers-client-auth-token',
   USER_NAME: 'lost-puppers-user-name',
   USER_ID: 'lost-puppers-user-id'
