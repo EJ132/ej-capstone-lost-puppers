7bfadba1bf8f2d45c477d32876c765788ce0518d
EJ <ejg132@gmail.com>
2019-10-21 15:21:12 -0700

    will be test data for the map integration

---
+++ src/Context/testData.js
@@ -0,0 +1,52 @@
+export const pupArray = [
+    {
+        name: 'Ricky',
+        description: 'Loves to play with toys',
+        cordinates: [33.899421, -118.356705]
+    },
+    {
+        name: 'Nicko',
+        description: 'Loves to play with toys',
+        cordinates: [33.913904, -118.349270]
+    },
+    {
+        name: 'Poppy',
+        description: 'Loves to play with toys',
+        cordinates: [33.914195, -118.366029]
+    },
+    {
+        name: 'Carlos',
+        description: 'Loves to play with toys',
+        cordinates: [33.902627, -118.327773]
+    },
+    {
+        name: 'Brad',
+        description: 'Loves to play with toys',
+        cordinates: [33.891844, -118.349385]
+    },
+    {
+        name: 'Albert',
+        description: 'Loves to play with toys',
+        cordinates: [33.886093, -118.358410]
+    },
+    {
+        name: 'Ivory',
+        description: 'Loves to play with toys',
+        cordinates: [33.915176, -118.377221]
+    },
+    {
+        name: 'Moose',
+        description: 'Loves to play with toys',
+        cordinates: [33.914760, -118.319538]
+    },
+    {
+        name: 'Jake',
+        description: 'Loves to play with toys',
+        cordinates: [33.896731, -118.347268]
+    },
+    {
+        name: 'Lucy',
+        description: 'Loves to play with toys',
+        cordinates: [33.886792, -118.344875]
+    },
+]
