bb3d6f765f45495f60285cf200aa1a03097a25b9
EJ <ejg132@gmail.com>
2019-10-23 12:42:32 -0700

    css update

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -6,6 +6,10 @@ import TokenService from '../../services/token-service'
 
 export default class Navbar extends Component {
 
+    state={
+        clicked: false
+    }
+
 handleLogoutClick = () => {
     TokenService.clearAuthToken()
     TokenService.clearUserName()
@@ -30,20 +34,29 @@ handleLogoutClick = () => {
     )
     }
 
+    onClick = () => {
+
+    }
+
     render () {
+
+
         
         return (
-            <ul className="navbar">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/aboutus">About Us</Link></li>
-                <li><Link to="/find">Find a Pupper</Link></li>
-                <li><Link to='/help'>Help</Link></li>
-                {TokenService.hasAuthToken() ? <li><Link to='/profile'>Profile</Link></li> : null}
-                <li>{TokenService.hasAuthToken()
-                    ? this.renderLogoutLink()
-                    : this.renderLoginLink()}</li>
-                <img alt="paw" src={paw}></img>
-            </ul>
+            <div>
+
+                <ul className="navbar">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/aboutus">About Us</Link></li>
+                    <li><Link to="/find">Find a Pupper</Link></li>
+                    <li><Link to='/help'>Help</Link></li>
+                    {TokenService.hasAuthToken() ? <li><Link to='/profile'>Profile</Link></li> : null}
+                    <li>{TokenService.hasAuthToken()
+                        ? this.renderLogoutLink()
+                        : this.renderLoginLink()}</li>
+                    <Link to='/'><img alt="paw" src={paw}></img></Link>
+                </ul>
+            </div>
         )
     }
 }
